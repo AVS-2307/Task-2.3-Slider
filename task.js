9b4bb3131c2a30d76e86e8ee316bf308f66d9996
AVS-2307 <andrey230781.an@gmail.com>
2023-02-21 13:10:45 +0300

    function activeSlide renew

--- task.js
+++ task.js
@@ -3,9 +3,8 @@ const sliderArrowNext = document.querySelector('.slider__arrow_next');
 const sliderArrowPrev = document.querySelector('.slider__arrow_prev');
 
 //создадим функцию поиска нужного слайда в массиве
-function activeSlide() {
-    const slideIsActive = 'slider__item_active';    
-    return slideIsActive;
+function activeSlide(slide) {        
+    return slide.classList.contains('slider__item_active');
 }
 
 let currentSlide = sliderImages.findIndex(activeSlide);
